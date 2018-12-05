6a4be9a94e59073967cb0ce958313f56b5f6133a
John Hauge <john@troop.work>
2018-12-05 10:41:38 -1000

    updates

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const forward = require('http-forward')
 
 var server = http.createServer(function (req, res) {
   console.log(req)
+  console.log('req.url', req.url)
+  console.log('req.method', req.method)
+  console.log('req.headers', req.headers)
   // Define proxy config params
   // req.forward = { target: 'http://new.server.net' }
   // forward(req, res)
+
+  res.end('ah ah ah')
 })
 
 server.listen(7001)
